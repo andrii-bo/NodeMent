7bc277340a856d48ccbded829a7688c2a5314878
Andrii Bondarenko <mail.bondarenki@gmail.com>
2019-12-03 01:09:44 +0200

    20191202.1
    master
    M	HW_1_2/result.json
    M	HW_1_2/task2.js

--- HW_1_2/task2.js
+++ HW_1_2/task2.js
@@ -1,10 +1,19 @@
 const { pipeline } = require('stream');
 const fs = require('fs');
-const csv=require('csvtojson');
+const csv = require('csvtojson');
 
 pipeline(
   fs.createReadStream('node_mentoring_t1_2_input_example.csv'),
-  csv(),  
+  csv({
+    headers: ['book', 'author', 'Amount', 'price'],
+    colParser: {
+      "book": "string",
+      "author": "string",
+      "Amount": "omit",
+      "price": "number",
+    },
+    trim: true
+  }),
   fs.createWriteStream('result.json'),
   (err) => {
     if (err) {
