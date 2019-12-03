700589d893c6cb240d175a8e31d4a8986ed42f42
Andrii Bondarenko <mail.bondarenki@gmail.com>
2019-12-04 01:07:28 +0200

    20191202.1
    master
    D	DATA_IN/node_mentoring_t1_2_input_example.csv
    D	DATA_OUT/hw_1_2_result.json
    M	HW_1_2/index.js
    M	HW_1_2/package.json
    M	run_hw_1_2.cmd

--- HW_1_2/index.js
+++ HW_1_2/index.js
@@ -2,13 +2,14 @@ let in_file = process.argv[2];
 let result_file = process.argv[3];
 
 if (in_file == null) {
-  in_file="HW_1_2\\index.js DATA_IN\\node_mentoring_t1_2_input_example.csv";
+  in_file="DATA_IN\\node_mentoring_t1_2_input_example.csv";
 }
 
 if (result_file == null) {
   result_file="DATA_OUT\\hw_1_2_result.json";
 }
 
+const path = require('path');
 const { pipeline } = require("stream");
 const fs = require("fs");
 const csv = require("csvtojson");
@@ -16,7 +17,7 @@ const csv = require("csvtojson");
 console.log("transform " + in_file + " to " + result_file);
 
 pipeline(
-  fs.createReadStream(in_file),
+  fs.createReadStream(path.resolve(__dirname, in_file)),
   csv({
     headers: ["book", "author", "Amount", "price"],
     colParser: {
@@ -27,7 +28,7 @@ pipeline(
     },
     trim: true
   }),
-  fs.createWriteStream(result_file),
+  fs.createWriteStream(path.resolve(__dirname, result_file)),
   err => {
     if (err) {
       console.error("Pipeline failed.", err);
--- run_hw_1_2.cmd
+++ run_hw_1_2.cmd
@@ -1,1 +1,1 @@
-call _run.cmd HW_1_2 DATA_IN\node_mentoring_t1_2_input_example.csv DATA_OUT\hw_1_2_result.json  
+call _run.cmd HW_1_2 DATA_IN\node_mentoring_t1_2_input_example.csv DATA_OUT\hw_1_2_result.json
