5f43331bf452b9e47243245444d7b7bb9df1aeb4
Andrii Bondarenko <mail.bondarenki@gmail.com>
2020-01-29 01:56:16 +0200

    20191204.1
    master
    M	HW_3_1/src/controllers/controllersSet.ts

--- HW_3_1/src/controllers/controllersSet.ts
+++ HW_3_1/src/controllers/controllersSet.ts
@@ -1,16 +1,17 @@
 import { UserController } from './userCtl';
 import express from "express";
 import { DatabaseProvider } from 'database';
+import { Controller } from './controller';
 
 export class ControllersSet {
     protected expApp: express.Application;
     protected db: DatabaseProvider;
-    controllers = [
-        new UserController(this.expApp,this.db)
-    ];
-    constructor(app: express.Application,db:DatabaseProvider) {
-        this.expApp = app;  
-        this.db = db;        
+    protected controllers: Controller[] = [];
+
+    constructor(expApp: express.Application, db: DatabaseProvider) {
+        this.expApp = expApp;
+        this.db = db;
+        this.controllers.push(new UserController(expApp, db));
     };
 
 }
